af203bfc866abf6813f053a9221547d402be11fa
guoxuan.wang <guoxuan.wang@transsion.com>
2023-04-07 18:09:54 +0800

    add: footer

---
+++ docs/.vitepress/theme/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer>
+    Powered by
+    <a href="https://github.com/HamsterCat-W/vitepress-blog" target="_blank" title="作者">HamsterCat-W</a>
+    | Copyright © 2019-{{ date }} | MIT License
+  </footer>
+</template>
+
+<script setup lang="ts">
+const date = new Date().getFullYear()
+</script>
+
+<style scoped lang="less">
+footer {
+  text-align: center;
+  position: absolute;
+  width: 100%;
+  bottom: 20px;
+  padding: 0 20px;
+  color: var(--vp-c-text);
+}
+a {
+  color: var(--vp-c-brand);
+  font-weight: bold;
+}
+a:hover {
+  text-decoration: none;
+  color: var(--vp-button-brand-hover-bg);
+}
+</style>
---
+++ docs/.vitepress/theme/layouts/BasicLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <Layout></Layout>
+    <div class="footer">
+      <Footer></Footer>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import DefaultTheme from 'vitepress/theme'
+import Footer from '../components/Footer.vue'
+import { useData } from 'vitepress'
+const { page, frontmatter } = useData()
+const { Layout } = DefaultTheme
+</script>
+
+<style scoped lang="less">
+.container {
+  position: relative;
+}
+</style>
